58d80aa51df33861b2b6f13b6f81e9b776b237f9
TaloeyToey <toey_and_guy@hotmail.com>
2019-05-24 15:49:32 +0700

    kuy

--- backend-js/Server.js
+++ backend-js/Server.js
@@ -13,83 +13,70 @@ var con = mysql.createConnection({
   user: "UserManage",
   password: "jsplus1",
   database: "jsplus",
-  // port: "8889",
-  // socketPath: "localhost:/Applications/MAMP/tmp/mysql/mysql.sock"
 });
 
 app.get('/users', (req, res) => {
   con.query('select concat(name," ",surname) as Name from User where Position_ID = 1', function (err, result, fields) {
     if (err) {
-      console.log(err)
+      console.log("/user : "+err)
       throw err
     };
-    console.log(result);
     res.json(result)
+    res.end
   });
-})
-
-app.get('/addperiod', (req,res) => {
-  con.query('select Period_Name,Period_Time_One,Period_Time_Two from Period', function (err,result,fields) {
-    if(err) {
-      console.log(err);
-      throw err
-    };
-    console.log(result);
-    res.json(result)
-  })
-})
-
-app.post('/period', (req, res) => {
-  console.log(req.body)
-  // res.send(200)
-  req.body.period.forEach(e => {
-    console.log(e);
-    con.query(`
-  insert into Period (Period_ID,Period_Name,Period_Time_One,Period_Time_Two) values('1',"${e.periodName}","${e.periodOne}","${e.periodTwo}")
-  `, function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-        res.json(result);
-      });
-  })
 })
 
 app.get('/showperiod', (req, res) => {
   con.query('select Period_Name,Period_Time_One,Period_Time_Two from Period', function (err, result, fields) {
     if(err){
-      console.log(err)
+      console.log("/showperiod : "+err)
       throw err
     };
-    console.log(result);
     res.json(result)
+    res.end
   })
 })
 
 app.get('/company', (req, res) => {
   con.query('select c.Company_Name from Company c join Department d on c.Company_ID = d.Company_ID join Position p on d.Department_ID = p.Department_ID where p.Position_Name = "Tester"', function (err, result, fields) {
     if (err) {
-      console.log(err)
+      console.log("/company : "+err)
       throw err
     };
-    console.log(result);
     res.json(result)
+    res.end
   });
 })
 
 app.get('/department', (req, res) => {
   con.query('select d.Department_Name from Company c join Department d on c.Company_ID = d.Company_ID join Position p on d.Department_ID = p.Department_ID where p.Position_Name = "Tester"', function (err, result, fields) {
     if (err) {
-      console.log(err)
+      console.log("/department : "+err)
       throw err
     };
-    console.log(result);
     res.json(result)
+    res.end
   });
+})
+
+app.post('/period', (req, res) => {
+  req.body.period.forEach(e => {
+    con.query(`
+  insert into Period (Period_Name,Period_Time_One,Period_Time_Two,Period_Color) values("${e.periodName}","${e.periodOne}","${e.periodTwo}","red")
+  `, function (err, result, fields) {
+        if (err) {
+          console.log("/period : "+err)
+          res.end(result)
+          throw err;
+        }
+        res.json(result);
+      });
+  })
 })
 
 con.connect(err => {
   app.listen(8080, () => {
-    console.log('Connection success, Start Mamp server at port 8080.')
+    console.log('Connection success, Start server at port 8080.')
   })
 })
 
