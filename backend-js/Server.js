44654b9e07b50f6fa455a7ac9357277fa0ef10b3
TaloeyToey <toey_and_guy@hotmail.com>
2019-09-04 15:56:54 +0700

    Login and Register and Upload

--- backend-js/Server.js
+++ backend-js/Server.js
@@ -5,24 +5,18 @@ app.use(cors())
 var mysql = require('mysql');
 require('dotenv').config();
 var bodyParser = require('body-parser')
+const jwt = require("jwt-simple");
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
 var con = mysql.createConnection({
-  // host: "13.59.253.146",
-  // user: "UserManage",
-  // password: "jsplus1",
-  // database: "jsplus"
-  //ในเฟช
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
 });
-
-// console.log(process.env.DB_HOST)
 
 app.get('/users', (req, res) => {
   con.query('select concat(name," ",surname) as Name,User_ID from User where Position_ID = 1', function (err, result, fields) {
@@ -31,7 +25,6 @@ app.get('/users', (req, res) => {
       throw err
     };
     res.json(result)
-    console.log(result);
   });
 })
 
@@ -91,7 +84,6 @@ app.get('/showschedule', (req, res) => {
 })
 
 app.post('/deleteperiod', async (req, res) => {
-  console.log(req.body.DeletePeriod)
   await con.query(`
   Delete from Schedule where Period_ID = "${req.body.DeletePeriod.Period_ID}"`, function (err, result, fields) {
       if (err) {
@@ -111,8 +103,6 @@ app.post('/deleteperiod', async (req, res) => {
 })
 
 app.post('/schedule', (req, res) => {
-  console.log("Accept")
-
   let insert = "INSERT INTO Schedule (User_ID, Date, Period_ID) VALUES ?"
   let values = [];
 
@@ -136,7 +126,6 @@ app.post('/schedule', (req, res) => {
 
 
 app.post('/schedule/delete', (req, res) => {
-  console.log(req.body.DeletePeriodDB)
   con.query(`Delete FROM Schedule where User_ID = "${req.body.DeletePeriodDB.User_ID}" and Date = "${req.body.DeletePeriodDB.Date}" and Period_ID = "${req.body.DeletePeriodDB.Period_ID}"`, function (err, result, fields) {
     if (err) {
       console.log("/deleteperiod : " + err)
@@ -144,6 +133,73 @@ app.post('/schedule/delete', (req, res) => {
     };
     res.json(result)
   })
+})
+
+app.post('/register', (req, res) => {
+  con.query(`INSERT INTO User (UserName, Password) VALUES ("${req.body.register.username}","${req.body.register.password}")`, function (err, result, fields) {
+    if (err) {
+      console.log("/register : " + err)
+      throw err
+    };
+    res.json(result);
+  })
+})
+
+app.post('/company/insert', (req, res) => {
+  con.query(`INSERT INTO Company (Company_Name, Company_Mail,Company_Tel,Company_Picture) VALUES ("${req.body.createcompany.companyName}","${req.body.createcompany.companyEmail}","${req.body.createcompany.companyTel}","${req.body.companypicture}")`, function (err, result, fields) {
+    if (err) {
+      throw err
+    };
+    res.json(result);
+  })
+})
+
+app.get('/company/select', (req, res) => {
+  con.query('select * from Company c where c.Company_ID = 11 ', function (err, result, fields) {
+    if (err) {
+      throw err
+    };
+    res.json(result)
+  });
+})
+
+/*------------------------------Login------------------------------------*/
+const CheckUsernameMiddleWare = (req, res, next) => {
+  var found = false;
+  Boolean(found);
+  con.query(`select username from User`, function (err, result, fields) {
+    result.forEach(e => {
+      if (e.username === req.body.username) {
+        found = true;
+        next();
+      }
+    })
+    if (found != true) {
+      res.json("Wrong Username or Password")
+    }
+  })
+}
+
+const LoginMiddleWare = (req, res, next) => {
+  con.query(`select username,password from User where username = "${req.body.username}"`, function (err, result, fields) {
+    if (err) {
+      throw err;
+    }
+    if (req.body.username === result[0].username && req.body.password == result[0].password) {
+      next();
+    } else {
+      res.json("Wrong Username or Password")
+    }
+  })
+};
+
+app.post("/users/authenticate", CheckUsernameMiddleWare, LoginMiddleWare, (req, res) => {
+  const payload = {
+    sub: req.body.username,
+    iat: new Date().getTime()
+  };
+  const SECRET = process.env.SECRETKEYS;
+  res.json({ tk: jwt.encode(payload, SECRET), name: req.body.username })
 })
 
 con.connect(err => {
